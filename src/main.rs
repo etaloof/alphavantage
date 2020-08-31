71678333eafd45a85331e67ec28045205e6af475
Stefan Lukas <etaloof@gmail.com>
2020-08-31 18:07:22 +0200

    Optimize feature flags

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use alphavantage::*;
 
 fn main() {
+    #[cfg(not(feature = "default"))]
+    use MockClient as UreqClient;
+
     let result = AlphavantageClient::new("demo", UreqClient).search_endpoint("test");
     dbg!(result);
 }
